075ef4ee66f178e25984b05436ca9a6ea4c0020c
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-11-17 18:02:14 +0330

    get elements in js

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const [playerOneCurrentScore, playerTwoCurrentScore] = document.getElementsByClassName('current')
+const [playerOneFinalScore, playerTwoFinalScore] = document.getElementsByClassName('final-score')
+const [newGameButton, rollDiceButton, holdButton] = document.getElementsByTagName('button')
+
