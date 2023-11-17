ec327b02036be94312a5e6afe7f82aef2bc06a0d
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-11-17 21:00:37 +0330

    complete roll dice and show the current score function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
+"use strict"
+
 const [playerOneCurrentScore, playerTwoCurrentScore] = document.getElementsByClassName('current')
 const [playerOneFinalScore, playerTwoFinalScore] = document.getElementsByClassName('final-score')
 const [newGameButton, rollDiceButton, holdButton] = document.getElementsByTagName('button')
+const diceImage = document.getElementsByTagName('img')
 
+let playerOneSumCurrentScore = 0
+let playerTwoSumCurrentScore = 0
+const diceNumber = [1]
+
+rollDiceButton.addEventListener('click', function() {
+    diceNumber.push(Math.floor(Math.random() * 6) + 1)
+    console.log(diceNumber)
+    playerOneSumCurrentScore += (diceNumber[diceNumber.length - 1])
+    playerOneCurrentScore.textContent = playerOneSumCurrentScore
+    diceImage[diceNumber[diceNumber.length - 2] - 1].style.display = 'none'
+    diceImage[diceNumber[diceNumber.length - 1] - 1].style.display = 'block'
+})
