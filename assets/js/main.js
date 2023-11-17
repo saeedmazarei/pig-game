46cb3352bd7e5df9a4cb6d5190da8731db17af2d
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-11-17 22:06:27 +0330

    complete new game button function

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,11 +10,22 @@ let playerOneSumCurrentScore = 0
 let playerOneTotalScore = 0
 let playerTwoSumCurrentScore = 0
 let playerTwoTotalScore = 0
-const diceNumber = [1]
+let diceNumber = [1]
 let turn = 1
 
 rollDiceButton.addEventListener('click', function() {
     diceNumber.push(Math.floor(Math.random() * 6) + 1)
+    diceImage[diceNumber[diceNumber.length - 2] - 1].style.display = 'none'
+    diceImage[diceNumber[diceNumber.length - 1] - 1].style.display = 'block'
+    if (diceNumber[diceNumber.length - 1] === 1 && turn === 1) {
+        playerOneSumCurrentScore = 0
+        holdHandler()
+        return
+    } else if (diceNumber[diceNumber.length - 1] === 1 && turn === 2) {
+        playerTwoSumCurrentScore = 0
+        holdHandler()
+        return
+    }
     if (turn === 1) {
         playerOneSumCurrentScore += (diceNumber[diceNumber.length - 1])
         playerOneCurrentScore.textContent = playerOneSumCurrentScore
@@ -22,11 +33,11 @@ rollDiceButton.addEventListener('click', function() {
         playerTwoSumCurrentScore += (diceNumber[diceNumber.length - 1])
         playerTwoCurrentScore.textContent = playerTwoSumCurrentScore
     }
-    diceImage[diceNumber[diceNumber.length - 2] - 1].style.display = 'none'
-    diceImage[diceNumber[diceNumber.length - 1] - 1].style.display = 'block'
 })
 
-holdButton.addEventListener('click', function() {
+holdButton.addEventListener('click', holdHandler)
+
+function holdHandler() {
     if (turn === 1) {
         playerOneCurrentScore.textContent = 0
         playerOneTotalScore += playerOneSumCurrentScore
@@ -43,5 +54,23 @@ holdButton.addEventListener('click', function() {
         turn = 1
         player2container.classList.remove('active')
         player1container.classList.add('active')
+    }
+}
+
+newGameButton.addEventListener('click', function() {
+    diceImage[diceNumber[diceNumber.length - 1] - 1].style.display = 'none'
+    playerOneSumCurrentScore = 0
+    playerOneTotalScore = 0
+    playerTwoSumCurrentScore = 0
+    playerTwoTotalScore = 0
+    playerOneFinalScore.textContent = 0
+    playerTwoFinalScore.textContent = 0
+    playerOneCurrentScore.textContent = 0
+    playerTwoCurrentScore.textContent = 0
+    diceNumber = [1]
+    if (turn === 2) {
+        player2container.classList.remove('active')
+        player1container.classList.add('active')
+        turn = 1
     }
 })
